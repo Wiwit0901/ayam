196e883893514654c19d1aea31588a4c2467c4ff
Wiwit thoyyibah <55557102+Wiwit0901@users.noreply.github.com>
2019-09-20 19:28:45 +0700

    ayam

---
+++ ayam/telor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+int x;
+int y;
+for (x=1;x<=10;x++){
+	cout <<endl;
+for (y=10;y>=x;y--){
+	cout << "ayam" << " " ;
+}
+}
+
+_getch ();
+return 0;
+}
